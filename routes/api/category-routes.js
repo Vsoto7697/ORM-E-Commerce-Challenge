cb38bc88737dfbe1cec0c56c4854a3c4f67f83fa
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-12-15 22:49:29 -0500

    using routed.get I was able to find one category by it's id value and included its associated products

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,6 +33,26 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(dbCategoryData => {
+      if(!dbCategoryData) {
+        res.status(404).json({message: 'No categories were found'});
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    });
 });
 
 router.post('/', (req, res) => {
