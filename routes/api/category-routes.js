63c96ee7d05f2468ce843402161c7dffea4ef73a
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-12-15 22:46:49 -0500

    found all categories and included its associated products using js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,9 +4,31 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+    // find all categories
+    // be sure to include its associated Products
+    Category.findAll({ 
+    include:
+        {
+          model: Product,
+          attributes: ['id','product_name','price','stock','category_id']
+        }
+    })
+
+    .then(dbCategoryData => {
+      if(!dbCategoryData) {
+        res.status(404).json({message: 'Sorry No categories were found'});
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    });
+
 });
+      
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
